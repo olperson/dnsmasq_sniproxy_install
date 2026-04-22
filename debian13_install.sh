7d5def8c91c5dde686c2f0aa684b4a9c4f26aa87
olperson <clearlove13z@gmail.com>
2026-04-22 22:18:23 +0800

    升级sniproxy

--- debian13_install.sh
+++ debian13_install.sh
@@ -364,9 +364,9 @@ install_sniproxy(){
     elif check_sys packageManager apt; then
         if [[ ${fastmode} = "1" ]]; then
             if [[ ${bit} = "x86_64" ]]; then
-                download /tmp/sniproxy_0.6.1_amd64.deb https://github.com/olperson/dnsmasq_sniproxy_install/raw/master/sniproxy/sniproxy_0.6.1_amd64.deb
-                error_detect_depends "dpkg -i --no-debsig /tmp/sniproxy_0.6.1_amd64.deb"
-                rm -f /tmp/sniproxy_0.6.1_amd64.deb
+                download /tmp/sniproxy_0.7.0-1_amd64.deb https://github.com/olperson/dnsmasq_sniproxy_install/raw/master/sniproxy/sniproxy_0.7.0-1_amd64.deb
+                error_detect_depends "dpkg -i --no-debsig /tmp/sniproxy_0.7.0-1_amd64.deb"
+                rm -f /tmp/sniproxy_0.7.0-1_amd64.deb
             else
                 echo -e "${red}暂不支持${bit}内核，请使用编译模式安装！${plain}" && exit 1
             fi
